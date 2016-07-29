8081bd7b8683f2f4f2e8187ba92d6d5d036b4d85
Paul Popoff <oc14@ya.ru>
2016-07-29 19:53:28 +0300

    Comment added, minor improvements

--- src/main/scala/setun/ternary/package.scala
+++ src/main/scala/setun/ternary/package.scala
@@ -1,18 +1,20 @@
 package setun
 
 /**
-  * Created by i on 2/2/16.
+  * This package contains Ternary class that
+  * contains Ternary logic and supported
+  * operations
   */
 package object ternary {
 
   sealed trait Ternary
-  case object True extends Ternary
+  case object True  extends Ternary
   case object False extends Ternary
   case object Maybe extends Ternary
 
-
   def unary_!(t: Ternary) = not(t)
   def ¬ (t: Ternary) = not(t)
+  def ait(t: Ternary) = not(t)
   def negate(t: Ternary) = not(t)
 
   def not (t: Ternary) = t match {
@@ -21,12 +23,10 @@ package object ternary {
     case False => True
   }
 
-  implicit class TernaryOps(t: Ternary) {
 
+  implicit class TernaryOps(t: Ternary) {
     def && (other: Ternary) = t and other
     def || (other: Ternary) = t or other
-//    def && (other: Ternary) = and(other)
-//    def && (other: Ternary) = and(other)
 
     def ∧ (other: Ternary) = t and other
     def ∨ (other: Ternary) = t or other
@@ -45,21 +45,17 @@ package object ternary {
       case (Maybe, False) => False
     }
 
-
     def or (other: Ternary) = (t, other) match {
       case (True, True) => True
       case (True, Maybe) => True
       case (True, False) => True
-
       case (Maybe, True)  => True
       case (Maybe, Maybe) => Maybe
       case (Maybe, False) => Maybe
-
       case (False, True)  => True
       case (False, Maybe) => Maybe
       case (False, False) => False
     }
-
 
     def equiv(other: Ternary) = (t, other) match {
       case (True, True)   => True
